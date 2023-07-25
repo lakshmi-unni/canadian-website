064aeb8e6ea51915b400c3b5e722f5cc55ec84aa
lakshmi unni <lakshmi@bluehour.app>
2023-07-25 13:47:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7958e3c (first commit)
 import './App.css';
 import Footer from './Components/Footer.js/Footer';
 import Header from './Components/Header/Header';
