7a5c8ece03e5e10e54066cb35cf8834776fb23f1
lakshmi unni <lakshmi@bluehour.app>
2023-07-27 22:54:43 +0530

    first commit

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -5,6 +5,7 @@ const Loader = () => {
     return (
         <div className="loader-container">
             <div className="loader"></div>
+            <div className="loader-text">Loading...</div>
         </div>
     );
 };
